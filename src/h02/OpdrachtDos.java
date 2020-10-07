2031d61d53386d1f1726e8e033f1cbcf6c5f628f
sebkijk <sebastiaan.khouw@hotmail.com>
2020-10-07 18:00:59 +0200

    Completed the second Assignment. I have written my first name in the color blue and I used the color red for my last name. I also put my last name under my first name!

--- src/h02/OpdrachtDos.java
+++ src/h02/OpdrachtDos.java
@@ -1,4 +1,22 @@
 package h02;
 
-public class OpdrachtDos {
+import java.applet.Applet;
+import java.awt.*;
+
+public class OpdrachtDos extends Applet {
+
+
+    public void init() {
+        setBackground(Color.white);
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.setColor(Color.blue);
+        g.drawString("Sebas ", 50, 50);
+        g.setColor(Color.red);
+        g.drawString("Khouw",50,65);
+
+    }
 }
