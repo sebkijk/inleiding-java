28f65e567010324a48cbcee0f9fbf4ea1865399b
sebkijk <sebastiaan.khouw@hotmail.com>
2020-10-07 13:17:11 +0200

    Changed text from "Hallo klas 8A!" to "Hello World!".

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo cohort 0!");
+        System.out.println("Hello World!");
     }
 }
