c303f15c977ff6a1b6af566f185ec1d61ae2212f
sebkijk <sebastiaan.khouw@hotmail.com>
2020-10-12 21:15:43 +0200

    Added the line for the practical assignment!

---
+++ src/h04/h04PO.java
@@ -0,0 +1,19 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class h04PO extends Applet {
+
+
+    public void init() {
+
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawLine(20,20,200,20);
+        g.drawString("Lijn",100,35);
+
+    }
+}
